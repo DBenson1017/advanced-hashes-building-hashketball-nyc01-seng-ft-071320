80096db035b99410d83bdf07b3bffd6db8e1cfd7
Devin Benson <devinbenson@gmail.com>
2020-07-06 15:40:29 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -64,7 +64,7 @@ away: {
     colors: ["Turquoise", "Purple"],
     players: [ 
         {
-      player_name: ,
+      player_name: 'Jeff Adrien ',
       number:   ,
       shoe:   ,
       points:    ,
